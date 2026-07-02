c7e3632d47fc06a6fb0b06a09d7dffaf0fb70a58
shayan paul <shayanpaul11c@gmail.com>
2026-07-02 11:04:02 +0530

    render deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,23 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "https://campusconnect-1-cvx3.onrender.com/api",
 });
 
-export default API;
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
 
-API.interceptors.request.use(
-  (req) => {
-    const token =
-      localStorage.getItem("token");
-
-    if (token) {
-      req.headers.Authorization =
-        `Bearer ${token}`;
-    }
-
-    return req;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return req;
+});
 
 export default API;
